e3ab77e2a91d608736374f8e11d308ac66c98fb4
jlandowner <jlandowner8@gmail.com>
2021-12-14 03:07:48 +0900

    update IngressPatch

--- base/krmfunc/ingress_patch.py
+++ base/krmfunc/ingress_patch.py
@@ -9,21 +9,43 @@ import logging
 def main(args=sys.argv[1:]):
   resource_list = yaml.load(sys.stdin, Loader=yaml.FullLoader)
 
-  certificate_arn = dict_get(resource_list, 'functionConfig.spec.certificate-arn')
-  host_name = dict_get(resource_list, 'functionConfig.spec.host')
+  target_name   = dict_get(resource_list, 'functionConfig.metadata.name')
+  annotations   = dict_get(resource_list, 'functionConfig.spec.annotations', {})
+
+  spec_hosts = dict_get(resource_list, 'functionConfig.spec.hosts', [])
+  hosts_config = []
+  for host in spec_hosts:
+    hosts_config.append({
+      'name': dict_get(host, 'name'),
+      'target': dict_get(host, 'target'),
+      'enableTls': dict_get(host, 'enableTls')
+    })
 
   output = []
   for resource in dict_get(resource_list, 'items', []):
-    if resource['apiVersion'] in ['extensions/v1beta1', 'networking.k8s.io/v1', 'networking.k8s.io/v1beta1'] and resource['kind'] == 'Ingress':
+    if resource['apiVersion'] in ['extensions/v1beta1', 'networking.k8s.io/v1', 'networking.k8s.io/v1beta1'] and resource['kind'] == 'Ingress' and dict_get(resource, 'metadata.name') == target_name:
       # patch annnotation
-      if certificate_arn:
-        ann = dict_get(resource, 'metadata.annotations', {})
-        ann['alb.ingress.kubernetes.io/certificate-arn'] = certificate_arn
-      
-      # patch host
-      if host_name:
-        for rule in dict_get(resource, 'spec.rules', []):
-          rule['host'] = host_name
+      if len(annotations) != 0:
+        ann = dict_get(resource, 'metadata.annotations')
+        if ann is not None:
+          for key in annotations:
+            ann[key] = annotations[key]
+        else:
+          resource['metadata']['annotations'] = annotations
+
+      for host_config in hosts_config:
+        host_name = dict_get(host_config, 'name')
+        target = dict_get(host_config, 'target')
+
+        # patch host
+        if host_name:
+          for rule in dict_get(resource, 'spec.rules', []):
+            if target is None:
+              rule['host'] = host_name
+            elif rule['host'] == target:
+              rule['host'] = host_name
+        
+        #TODO patch tls
 
     output.append(resource)
   sys.stdout.write(yaml.dump_all(output))

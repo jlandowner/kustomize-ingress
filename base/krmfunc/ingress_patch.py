ac6cb93d01f7d581f91a839f6b7e72b3b910cd3e
jlandowner <jlandowner8@gmail.com>
2021-12-14 03:19:13 +0900

    update IngressPatch

--- base/krmfunc/ingress_patch.py
+++ base/krmfunc/ingress_patch.py
@@ -12,20 +12,17 @@ def main(args=sys.argv[1:]):
   target_name   = dict_get(resource_list, 'functionConfig.metadata.name')
   annotations   = dict_get(resource_list, 'functionConfig.spec.annotations', {})
 
-  spec_hosts = dict_get(resource_list, 'functionConfig.spec.hosts', [])
-  hosts_config = []
-  for host in spec_hosts:
-    hosts_config.append({
+  hosts = []
+  for host in dict_get(resource_list, 'functionConfig.spec.hosts', []):
+    hosts.append({
       'name': dict_get(host, 'name'),
-      'target': dict_get(host, 'target'),
-      'enableTls': dict_get(host, 'enableTls')
-    })
+      'target': dict_get(host, 'target') })
 
   output = []
   for resource in dict_get(resource_list, 'items', []):
     if resource['apiVersion'] in ['extensions/v1beta1', 'networking.k8s.io/v1', 'networking.k8s.io/v1beta1'] and resource['kind'] == 'Ingress' and dict_get(resource, 'metadata.name') == target_name:
       # patch annnotation
-      if len(annotations) != 0:
+      if len(annotations) > 0:
         ann = dict_get(resource, 'metadata.annotations')
         if ann is not None:
           for key in annotations:
@@ -33,17 +30,14 @@ def main(args=sys.argv[1:]):
         else:
           resource['metadata']['annotations'] = annotations
 
-      for host_config in hosts_config:
-        host_name = dict_get(host_config, 'name')
-        target = dict_get(host_config, 'target')
+      for host in hosts:
+        host_name = dict_get(host, 'name')
+        target = dict_get(host, 'target')
 
         # patch host
-        if host_name:
-          for rule in dict_get(resource, 'spec.rules', []):
-            if target is None:
-              rule['host'] = host_name
-            elif rule['host'] == target:
-              rule['host'] = host_name
+        for rule in dict_get(resource, 'spec.rules', []):
+          if target is None or rule['host'] == target:
+            rule['host'] = host_name
         
         #TODO patch tls
 
